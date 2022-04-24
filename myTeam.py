864393f5219f5b8592e81cc293691ccb5c885ec5
blakelawyer <blakethelawyer@gmail.com>
2022-04-24 11:20:21 -0500

    friendly tournament submission

--- myTeam.py
+++ myTeam.py
@@ -17,10 +17,10 @@ import random, time, util
 from game import Directions
 import game
 
-# Test
+
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first='OffenseAgent', second='DefenseAgent'):
+               first='OffenseAgent', second='OffenseAgent'):
     return [eval(first)(firstIndex), eval(second)(secondIndex)]
 
 
@@ -30,19 +30,17 @@ class ParentAgent(CaptureAgent):
         d = self.distancer.getDistance(pos1, pos2)
         return d
 
-
-    def aStarEat(self, game):
+    def aStar(self, game, start, goal):
 
         visited = []  # Visited list to prevent expanding a node multiple times.
         astar_priority_queue = util.PriorityQueue()
-        astar_priority_queue.push((game.getAgentPosition(self.index), [], 0), 0)
+        astar_priority_queue.push((start, [], 0), 0)
 
         while not astar_priority_queue.isEmpty():
             node = astar_priority_queue.pop()
             current_position = node[0]
             path = node[1]
-            # if current_position == goal:
-            if game.getRedFood()[node[0][0]][node[0][1]]:
+            if current_position == goal:
                 return path
             else:
                 if current_position not in visited:
@@ -65,6 +63,44 @@ class ParentAgent(CaptureAgent):
                             astar_priority_queue.push(((s[0], s[1]), path_history,
                                                        self.getMazeDistance(current_position, (s[0], s[1]))),
                                                       self.getMazeDistance(current_position, (s[0], s[1])))
+
+
+    def aStarEat(self, game):
+
+        visited = []  # Visited list to prevent expanding a node multiple times.
+        astar_priority_queue = util.PriorityQueue()
+        astar_priority_queue.push((game.getAgentPosition(self.index), [], 0), 0)
+
+        while not astar_priority_queue.isEmpty():
+            node = astar_priority_queue.pop()
+            current_position = node[0]
+            path = node[1]
+            if self.red:
+                if game.getBlueFood()[node[0][0]][node[0][1]]:
+                    return path, (node[0][0], node[0][1])
+            elif self.blue:
+                if game.getRedFood()[node[0][0]][node[0][1]]:
+                    return path, (node[0][0], node[0][1])
+            if current_position not in visited:
+                visited.append(current_position)
+                successors = []
+                x, y = node[0]
+                if not game.hasWall(x - 1, y) and (x - 1, y) not in visited:
+                    successors.append((x - 1, y, 'West'))
+                if not game.hasWall(x + 1, y) and (x + 1, y) not in visited:
+                    successors.append((x + 1, y, 'East'))
+                if not game.hasWall(x, y - 1) and (x, y - 1) not in visited:
+                    successors.append((x, y - 1, 'South'))
+                if not game.hasWall(x, y + 1) and (x, y + 1) not in visited:
+                    successors.append((x, y + 1, 'North'))
+                for s in successors:
+                    if (s[0], s[1]) not in visited:
+                        path_history = []
+                        path_history.extend(node[1])
+                        path_history.extend([s[2]])
+                        astar_priority_queue.push(((s[0], s[1]), path_history,
+                                                   self.getMazeDistance(current_position, (s[0], s[1]))),
+                                                  self.getMazeDistance(current_position, (s[0], s[1])))
 
 
     def aStarReturn(self, game, midpoint):
@@ -110,25 +146,72 @@ class OffenseAgent(ParentAgent):
 
         if gameState.isOnRedTeam(self.index):
             self.midpoint = 16
+            self.red = True
+            self.blue = False
         else:
             self.midpoint = 17
+            self.red = False
+            self.blue = True
+
+        self.at_midpoint = False
 
     def chooseAction(self, gameState):
-        actions = gameState.getLegalActions(self.index)
 
-
-        collected_pellets = 0
-        # game.getRedFood()[node[0][0]][node[0][1]]:
         x, y = gameState.getAgentPosition(self.index)
-        if gameState.getAgentState(self.index).numCarrying == 5:
+        if x == self.midpoint:
+            self.at_midpoint = True
+
+        if self.red:
+            enemy_indices = gameState.getBlueTeamIndices()
+        elif self.blue:
+            enemy_indices = gameState.getRedTeamIndices()
+        enemy_positions = []
+        for i in enemy_indices:
+            enemy_positions.append(gameState.getAgentPosition(i))
+
+
+        if not self.at_midpoint:
             path = self.aStarReturn(gameState, self.midpoint)
+            if path:
+                direction = path[0]
+                return direction
         else:
-            path = self.aStarEat(gameState)
+            path, closest_pellet = self.aStarEat(gameState)
+            enemy1_path = self.aStar(gameState, enemy_positions[0], closest_pellet)
+            enemy2_path = self.aStar(gameState, enemy_positions[1], closest_pellet)
+            if (len(path) < len(enemy1_path)) and (len(path) < len(enemy2_path)):
+                if path:
+                    direction = path[0]
+                    print("Going for pellet!")
+                    return direction
+
+        if self.red:
+            if x <= 16:
+                for enemy in enemy_positions:
+                    if enemy[0] <= 16:
+                        path = self.aStar(gameState, gameState.getAgentPosition(self.index), enemy)
+                        print("Going for enemy!")
+            else:
+                path = self.aStarReturn(gameState, self.midpoint)
+                print("Going for home!")
+                print(x, y)
+        elif self.blue:
+            if x >= 17:
+                for enemy in enemy_positions:
+                    if enemy[0] >= 17:
+                        path = self.aStar(gameState, gameState.getAgentPosition(self.index), enemy)
+                        print("Going for enemy!")
+            else:
+                path = self.aStarReturn(gameState, self.midpoint)
+                print("Going for home!")
+                print(x, y)
+
         if path:
             direction = path[0]
             return direction
         else:
-            return random.choice(actions)
+            return 'Stop'
+
 
 
 class DefenseAgent(ParentAgent):
@@ -147,7 +230,8 @@ class DefenseAgent(ParentAgent):
 
 
         actions = gameState.getLegalActions(self.index)
-        return random.choice(actions)
+        return 'Stop'
+        # return random.choice(actions)
 
         collected_pellets = 0
         # game.getRedFood()[node[0][0]][node[0][1]]:
